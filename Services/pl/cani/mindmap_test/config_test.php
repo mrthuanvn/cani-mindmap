c5b1bf7838d469f290803fd616479c04937bcf09
vanjan <vanjan@58dfb99b-a72c-0410-82b3-31752c4188f4>
2007-04-07 12:24:40 +0000

    phing

---
+++ Services/pl/cani/mindmap_test/config_test.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Created on 2007-04-03
+ *
+ * @author Jan Klosinski (Can I) vanjan@gmail.com
+ */
+ 
+define( "WebOrbServicesPath", "C:\\Program Files\\xampp\\htdocs\\Services\\" );
+ 
+?>
